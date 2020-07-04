681423d7a7ffd3d2d4f609138d1e24b8553ed972
Nima Yonten <nimayonten@gmail.com>
2020-07-04 14:52:01 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      backgroundColor: Colors.red,
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.red,
+        title: Text('Dice'),
+      ),
+      body: DicePage(),
+    ),
+  ));
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDice = 1;
+  int rightDice = 1;
+
+  void changeDice = () {};
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  leftDice = Random().nextInt(6) + 1;
+                  rightDice = Random().nextInt(6) + 1;
+                });
+              },
+              child: Image(
+                image: AssetImage('images/dice$leftDice.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  leftDice = Random().nextInt(6) + 1;
+                  rightDice = Random().nextInt(6) + 1;
+                });
+              },
+              child: Image(
+                image: AssetImage('images/dice$rightDice.png'),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
